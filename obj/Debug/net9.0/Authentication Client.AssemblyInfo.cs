7d44689708077fc98ed0c1a0823b717996aa4f8a
MOUGHIT <abdelmoughith2002@gmail.com>
2025-01-03 17:46:14 +0100

    no api

--- obj/Debug/net9.0/Authentication Client.AssemblyInfo.cs
+++ obj/Debug/net9.0/Authentication Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fced6e4167cf5231e92ca8db60833e9c10e76884")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
